978b0f52a2880f6b4f7fed4e92a6404af5c16628
DatenkrakeWasTaken <48020377+DatenkrakeWasTaken@users.noreply.github.com>
2022-07-08 19:23:27 +0200

    trimming integriert
    levenshtein trimming in main.py integriert

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from re import L
 import pandas as pd
 from rai_app import suggests
 import os
+from functools import lru_cache
 
 
 def load_data(file):
@@ -18,6 +19,55 @@ def get_user_input():
   
 def export_to_csv(df, path, sep=';'):
     df.to_csv(path, sep)
+
+def lev_dist(a, b):
+    '''
+    This function will calculate the levenshtein distance between two input
+    strings a and b
+    
+    params:
+        a (String) : The first string you want to compare
+        b (String) : The second string you want to compare
+        
+    returns:
+        This function will return the distnace between string a and b.
+        
+    example:
+        a = 'stamp'
+        b = 'stomp'
+        lev_dist(a,b)
+        >> 1.0
+    '''
+    
+    @lru_cache(None)  # for memorization
+    def min_dist(s1, s2):
+
+        if s1 == len(a) or s2 == len(b):
+            return len(a) - s1 + len(b) - s2
+
+        # no change required
+        if a[s1] == b[s2]:
+            return min_dist(s1 + 1, s2 + 1)
+
+        return 1 + min(
+            min_dist(s1, s2 + 1),      # insert character
+            min_dist(s1 + 1, s2),      # delete character
+            min_dist(s1 + 1, s2 + 1),  # replace character
+        )
+
+    return min_dist(0, 0)
+
+#source: https://towardsdatascience.com/text-similarity-w-levenshtein-distance-in-python-2f7478986e75
+
+def lev_dist_row(row):
+    scores = []
+    for root_word in row.root.split():
+        for target_word in row.target.split():
+            scores.append(lev_dist(root_word, target_word))
+    if any(score<3 for score in scores):
+        return row.target
+    else:
+        return 'delete'
 
 def main():
     import_path = 'rai_app/import_file/import.xlsx'
@@ -42,7 +92,12 @@ def main():
         except AttributeError:
             pass
 
+    df['target'] = df.apply(lev_dist_row, axis=1)
+    df = df.loc[df.target!='delete']
+    
+
     # Export
+    df = df.reset_index(drop=True)
     df.to_csv(export_path+'export.csv', sep=';', encoding='utf-8-sig')
     df.to_pickle(export_path+'export.pickle')
     df.to_json(export_path+'export.json')
